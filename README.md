7e350dabd65748d8a92b8f5533c78565df7e7b98
Raphael Brandão <nobre.roots@gmail.com>
2022-05-27 13:40:48 -0300

    atualizações

--- README.md
+++ README.md
@@ -2,24 +2,4 @@
 
 ---
 
-### Requisitos
-
-- [x] Deve ser possível criar uma conta
-- [x] Deve ser possível buscar o extrato bancário do cliente
-- [x] Deve ser possível realizar um depósito
-- [x] Deve ser possível realizar um saque
-- [] Deve ser possível buscar o extrato bancário do cliente por data
-- [] Deve ser possível atualizar os dados da conta do cliente
-- [] Deve ser possível obter dados da conta do cliente
-- [] Deve ser possível deletar uma conta
-
----
-
-### Regras de Negócio
-
-- [x] Não deve ser possível cadastrar uma conta com CPF já existente
-- [x] Não deve ser possível fazer depósito numa conta não existente
-- [x] Não deve ser possível buscar extrato numa conta que não existe
-- [x] Não deve ser possível fazer saque numa conta que não existe
-- [] Não deve ser possível excluir uma conta que não existe
-- [] Não deve ser possível fazer saque quando o saque for insuficiente
+Aplicativo básico de finanças desenvolvido em NodeJS
--- src/index.js
+++ src/index.js
@@ -112,4 +112,47 @@ app.post("/withdraw", verifyIfExistsAccountCpf, (request, response) => {
     return response.status(201).send()
 })
 
+app.get("/statement/date", verifyIfExistsAccountCpf, (request, response) => {
+    const { customer } = request
+    const { date } = request.query
+
+    const dateFormat = new Date(date + " 00:00").toDateString()
+
+    const statements = customer.statement.filter((statement) =>
+        statement.created_at.toDateString() === dateFormat
+    )
+
+    return response.json(statements)
+})
+
+app.put("/account", verifyIfExistsAccountCpf, (request, response) => {
+    const { name } = request.body
+    const { customer } = request
+
+    customer.name = name
+
+    return response.status(201).send()
+})
+
+app.get("/account", verifyIfExistsAccountCpf, (request, response) => {
+    const { customer } = request
+
+    return response.json(customer)
+})
+
+app.delete("/account", verifyIfExistsAccountCpf, (request, response) => {
+    const { customer } = request
+
+    customers.splice(customer, 1)
+
+    return response.status(200).json(customers)
+})
+
+app.get("/balance", verifyIfExistsAccountCpf, (request, response) => {
+    const { customer } = request
+    const balance = getBalance(customer.statement)
+
+    return response.json(balance)
+})
+
 app.listen(3333)
